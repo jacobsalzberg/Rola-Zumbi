54f29c832492a6ff99fce5bb553f0f86187cabd1
Jacob J. Salzberg <jacob1818@gmail.com>
2018-02-06 20:23:43 -0200

    Add GameManager.cs

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+	public GameObject selectedZombie;
+	public List<GameObject> zombies; 
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	// Use this for initialization
+	void Start () {
+		SelectZombie (selectedZombie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SelectZombie (GameObject newZombie) {
+		newZombie.transform.localScale = selectedSize;
+	}
+}
